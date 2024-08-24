0eb6acce0a8f92f60270301171444411770ad9fd
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-24 15:26:02 +0200

    Set up simple frontend with routers

---
+++ frontend/src/pages/moviepage/MoviePage.tsx
@@ -0,0 +1,7 @@
+export default function MoviePage() {
+    return (
+        <>
+            <h3>Here is the movie page</h3>
+        </>
+    )
+}
---
+++ frontend/src/pages/welcompage/WelcomePage.tsx
@@ -0,0 +1,6 @@
+export default function WelcomePage() {
+
+    return (
+        <h3>Welcome to the app !</h3>
+    )
+}
