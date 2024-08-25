050699d562829f21c69c5deb288b49930514ad0c
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-25 12:16:43 +0200

    Add link to movie page

--- frontend/src/pages/welcompage/WelcomePage.tsx
+++ frontend/src/pages/welcompage/WelcomePage.tsx
@@ -1,6 +1,11 @@
+import {Link} from "react-router-dom";
+
 export default function WelcomePage() {
 
     return (
-        <h3>Welcome to the app !</h3>
+        <>
+            <h3>Welcome to the app !</h3>
+            <Link to={"/movie"}>Go to Movie-Page</Link>
+        </>
     )
 }
