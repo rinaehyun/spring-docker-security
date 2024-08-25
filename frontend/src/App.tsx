77724b4482ab45af684dfeb8141bb9ca6f5a3fc6
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-25 12:15:27 +0200

    Install axios and fetch data when the button is clicked

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,29 @@ import './App.css'
 import WelcomePage from "./pages/welcompage/WelcomePage.tsx";
 import MoviePage from "./pages/moviepage/MoviePage.tsx";
 import {Route, Routes} from "react-router-dom";
+import {useState} from "react";
+import {Movie} from "./types/Movie.ts";
+import axios from "axios";
 
 function App() {
+    const [movieData, setMovieData] = useState<Movie[]>([]);
+
+    const fetchMovies = (): void => {
+        axios.get('/api/movie')
+            .then(response => {
+                setMovieData(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+            });
+    };
 
     return (
     <>
         <main>
             <Routes>
                 <Route path={"/"} element={<WelcomePage />}></Route>
-                <Route path={"/movie"} element={<MoviePage />}></Route>
+                <Route path={"/movie"} element={<MoviePage fetchMovies={fetchMovies} movieData={movieData}/>}></Route>
             </Routes>
         </main>
     </>
